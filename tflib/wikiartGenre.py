8a430c225c38f2b0aac1d31dbfd474ddc63bb6c1
Pablo <pabloizaya@gmail.com>
2019-11-28 20:11:58 +0000

    Update wikiartGenre.py

--- tflib/wikiartGenre.py
+++ tflib/wikiartGenre.py
@@ -13,20 +13,20 @@ DIM = 64
 path = os.path.normpath('/content/GANGogh/small/smallimages/images')
 
 #This dictionary should be updated to hold the absolute number of images associated with each genre used during training
-styles = {'abstract': 14794,
-          'animal-painting': 1319,
-          'cityscape': 5833,
-          'figurative': 3335,
-          'flower-painting': 1260,
-          'genre-painting': 14881,
-          'landscape': 14893,
-          'marina': 1199,
-          'mythological-painting': 1670,
-          'nude-painting-nu': 2276,
-          'portrait': 14496,
-          'religious-painting': 7915,
-          'still-life': 2314,
-          'symbolic-painting': 2454}
+styles = {'abstract': 14999,
+          'animal-painting': 1798,
+          'cityscape': 6598,
+          'figurative': 4499,
+          'flower-painting': 1800,
+          'genre-painting': 14997,
+          'landscape': 14999,
+          'marina': 1800,
+          'mythological-painting': 2099,
+          'nude-painting-nu': 3000,
+          'portrait': 14996,
+          'religious-painting': 8393,
+          'still-life': 2996,
+          'symbolic-painting': 2999}
 
 styleNum = {'abstract': 0,
             'animal-painting': 1,
